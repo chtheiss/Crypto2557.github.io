3acc7b619a083fefbeb41449c0a87c294b04e205
theiss <christoph.theiss@uni-jena.de>
2018-12-22 20:34:56 +0100

    Fixed a bug where the main page was not found

--- app/core/routes.py
+++ app/core/routes.py
@@ -5,7 +5,6 @@ from flask import (render_template, flash, redirect, url_for,
 from app.core import bp
 
 @bp.route('/', methods=['GET', 'POST'])
-@bp.route('/index/', methods=['GET', 'POST'])
 def index():   
     return render_template('index.html', title='Home')
 
