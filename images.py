3612f2fb9f21841ffca66f7eaa9b1429c718f763
Christoph Theiß <theisz.cm@googlemail.com>
2019-01-20 21:39:37 +0100

    Reverted to commit 34123ac4

---
+++ images.py
@@ -0,0 +1,9 @@
+import urllib.request
+
+for i in range(1,197):
+    try:
+        f = open('{}.png'.format(i), 'wb')
+        f.write(urllib.request.urlopen('https://www.endlessfrontierdata.com/images/units/{}'.format(i)).read())
+        f.close()
+    except:
+        print('failed {}'.format(i))
