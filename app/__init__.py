0038b43b3b909f996b0f439f1c2b87c5cbafdaf7
Christoph Theiß <theisz.cm@googlemail.com>
2019-01-20 22:18:03 +0100

    Closes #28. No more js in html files.

--- app/__init__.py
+++ app/__init__.py
@@ -19,9 +19,6 @@ def create_app(config_class=Config):
     jsglue = JSGlue(app)
     bootstrap.init_app(app)
 
-    from app.errors import bp as errors_bp
-    app.register_blueprint(errors_bp)
-
     from app.core import bp as core_bp
     app.register_blueprint(core_bp)
 
--- app/farming.py
+++ app/farming.py
@@ -7,14 +7,14 @@ app, freezer, pages = create_app()
 
 @freezer.register_generator
 def get_pet():
-	pets_url = os.path.join(app.root_path, "static", "pets.json")
+	pets_url = os.path.join(app.root_path, "static/json", "pets.json")
 	pets = json.load(open(pets_url))
 	for pet in pets.keys():
 		yield url_for('core.get_pet', petid=pet.replace(" ", "_"))
 
 @freezer.register_generator
 def get_unit():
-	units_url = os.path.join(app.root_path, "static", "units.json")
+	units_url = os.path.join(app.root_path, "static/json", "units.json")
 	units = json.load(open(units_url))
 	for unit in units.keys():
 		yield url_for('core.get_unit', unitid=unit.replace(" ", "_"))
