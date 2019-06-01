a76a8bb32422a3018ec5c2397c69f02323a6ab81
James Skemp <strivinglife@gmail.com>
2019-06-01 10:43:41 -0500

    Add missing pet and unit images

--- images.py
+++ images.py
@@ -1,6 +1,6 @@
 import urllib.request
 
-for i in range(1,197):
+for i in range(1,222):
     try:
         f = open('{}.png'.format(i), 'wb')
         f.write(urllib.request.urlopen('https://www.endlessfrontierdata.com/images/units/{}'.format(i)).read())
