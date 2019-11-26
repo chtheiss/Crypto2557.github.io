5cdd4937bbae7edf26718d927e31b9ffdd894fbf
Scott Hunt <schunt@adobe.com>
2019-11-26 11:01:13 -0800

    Update images.py download helper to fetch pets too

--- images.py
+++ images.py
@@ -1,9 +1,42 @@
+import json
+import os
 import urllib.request
 
-for i in range(1,197):
-    try:
-        f = open('{}.png'.format(i), 'wb')
-        f.write(urllib.request.urlopen('https://www.endlessfrontierdata.com/images/units/{}'.format(i)).read())
-        f.close()
-    except:
-        print('failed {}'.format(i))
+
+pets = json.load(open("app/static/json/pets.json"))
+pets_hard = json.load(open("app/static/json/hard_sh_pets.json"))
+units = json.load(open("app/static/json/units.json"))
+
+
+def fetch_image(type, filename):
+    target_file = "app/static/img/" + type + "/" + filename
+    source_url = "https://www.endlessfrontierdata.com/images/" + type + "/" + filename
+
+    if not os.path.isfile(target_file):
+        try:
+            print('Retrieving: {} from {}'.format(target_file, source_url))
+            f = open(target_file, 'wb')
+            f.write(urllib.request.urlopen(source_url).read())
+            f.close()
+        except:
+            print('ERROR: failed to retrieve: {}'.format(source_url))
+
+
+# units
+for key, unit in units.items():
+    img_file = os.path.basename(unit["img"])
+    fetch_image("units", img_file)
+
+    img_file_sr = os.path.basename(unit["img_sr"])
+    fetch_image("units", img_file_sr)
+
+# pets
+for key, pet in pets.items():
+    img_file = os.path.basename(pet["img"])
+    fetch_image("pets", img_file)
+
+# hard pets
+for key, pet in pets_hard.items():
+    img_file = os.path.basename(pet["img"])
+    fetch_image("pets", img_file)
+
