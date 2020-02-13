f27dc912960b25c1f3a4fc1f77db6e6e83d6d1b6
Crypto2557 <theisz.cm@googlemail.com>
2020-02-13 23:19:39 +0100

    Added unit images to pet descriptions

---
+++ tools/get_unit_id.py
@@ -0,0 +1,32 @@
+import os
+import json
+import re
+ids = []
+
+def remove_values_from_list(the_list, val):
+   return [value for value in the_list if value != val]
+
+
+
+pet_file = '../app/static/json/pets.json'
+with open(pet_file, 'r') as json_file:
+    pets = json.load(json_file)
+
+
+unit_file = '../app/static/json/units.json'
+with open(unit_file, 'r') as json_file:
+    units = json.load(json_file)
+
+
+unit_names = []
+for key, items in pets.items():
+    m = re.search('\[.*\]', items['description']['skill3'])
+    unit_name = m.group(0)[9:-2]
+    try:
+        items["couple"] = units[unit_name]['img_sr'].split("/")[-1].split(".")[0]
+    except:
+        print(unit_name)
+
+os.remove(pet_file)
+with open(pet_file, 'w') as json_file:
+    json.dump(pets, json_file, indent=4)
